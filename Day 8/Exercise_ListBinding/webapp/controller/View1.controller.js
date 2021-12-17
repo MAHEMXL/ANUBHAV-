ee0c0519eb28e36a665e3bd2d3d2732b73e5bfc6
soyuztechnologies <71425689+soyuztechnologies@users.noreply.github.com>
2021-12-17 18:11:30 +0530

    Update View1.controller.js

--- Day 8/Exercise_ListBinding/webapp/controller/View1.controller.js
+++ Day 8/Exercise_ListBinding/webapp/controller/View1.controller.js
@@ -15,6 +15,15 @@ sap.ui.define([
             var oAppCon = this.getView().getParent();
             //Step 2: From the parent App Con, navigate to view2
             oAppCon.to("idView2");
+        },
+        onDelete:function(oEvent){
+            //Step 1: which item user want to delete
+            var itemToBeDeleted = oEvent.getParameter("listItem");
+            //Step 2: We get the list object
+            var oList = oEvent.getSource();
+            //Step 3: Delete the item from list
+            oList.removeItem(itemToBeDeleted);
+
         }
     });
 });
